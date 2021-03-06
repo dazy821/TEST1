1a1bcce3543d22e3546eb5fd6d318903caf9d5a8
Kayla Ribeiro <kaylaribeiro0@gmail.com>
2021-03-06 09:35:14 -0500

    Homework 5

---
+++ functions.js
@@ -0,0 +1,26 @@
+let a = 3;
+let b = 4;
+
+
+function additonProblem (x, y) {
+	return x+y;
+}
+additionProblem(a, b);
+
+
+function subtractionProblem (x, y) {
+	return x-y;
+}
+subtractionProblem (a,b);
+
+
+function multiplicationProblem (x,y) {
+    return x*y
+}
+multiplicationProblem (a,b);
+
+
+function divisionProblem (x,y) {
+    return x/y
+}
+divisionProblem (a,b);
