5857637ed402188ca2255d16a4d9d591b5ddd9bd
Kayla Ribeiro <kaylaribeiro0@gmail.com>
2021-03-19 18:00:26 -0400

    Audio Recording

--- recordAudio.js
+++ recordAudio.js
@@ -1,10 +1,8 @@
 const AudioRecorder = require ('osx-audio');
 const fs = require('fs');
  
-function myFunc(input) {
-input= new AudioRecorder.Input();
+const input = new AudioRecorder.Input();
+ 
 const writable = fs.createWriteStream('output.wav');
 input.pipe(writable);
-}
 
-setTimeout(myFunc, 1500, 'time');
