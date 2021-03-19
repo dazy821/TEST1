9c5dd6f4fb1fbf39f865aaee482b6946f80ce19f
Kayla Ribeiro <kaylaribeiro0@gmail.com>
2021-03-19 18:11:03 -0400

    Audio Recording

--- recordAudio.js
+++
@@ -1,8 +0,0 @@
-const AudioRecorder = require ('osx-audio');
-const fs = require('fs');
- 
-const input = new AudioRecorder.Input();
- 
-const writable = fs.createWriteStream('output.wav');
-input.pipe(writable);
-
